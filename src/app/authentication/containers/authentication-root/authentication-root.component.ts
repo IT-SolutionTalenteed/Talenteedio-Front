8cf7ddb88214f69d51370787d91a6d6551985c32
Happy-Franck <solofonirina35@gmail.com>
2025-12-03 01:41:00 +0300

    Add responsive layout for company plan selection

--- src/app/authentication/containers/authentication-root/authentication-root.component.ts
+++ src/app/authentication/containers/authentication-root/authentication-root.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
     selector: 'app-authentication-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
     styleUrls: ['./authentication-root.component.scss'],
 })
 export class AuthenticationRootComponent {
+    isFullWidth = false;
 
+    constructor(private router: Router) {
+        this.router.events
+            .pipe(filter(event => event instanceof NavigationEnd))
+            .subscribe((event) => {
+                if (event instanceof NavigationEnd) {
+                    this.isFullWidth = event.url.includes('/company-plan');
+                }
+            });
+        
+        // Check initial route
+        this.isFullWidth = this.router.url.includes('/company-plan');
+    }
 }
