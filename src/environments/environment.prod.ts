6d2bdbe6731f906ea099aa03e6e656ceb89b0e78
Happy-Franck <solofonirina35@gmail.com>
2026-01-29 20:17:43 +0300

    env update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
   production: true,
-  // URLs locales pour Docker
-  publicBaseUrl: 'https://www.talenteed.io/public',
-  apiBaseUrl: 'https://www.talenteed.io/api',
+  publicBaseUrl: 'https://talenteed.io/public',
+  apiBaseUrl: 'https://talenteed.io/api',
   backOfficeBaseUrl: 'https://talenteed.io/admin',
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://talenteed.io',
   siteKey: '6LeLpugnAAAAANy32tV8QyXokyO1q7HgCZUE-hLR',
   googleAnalytics: 'G-ME05LJVVQ2',
-  stripePublicKey: 'pk_test_51SU6lA8RbHgy6D1m3Litx37vWguQEYbYkSu5V7IAvoDHUVkslR0SwDPvx82hufR4Ol3PqFINEOh2GeQ6Ijt7jHC000Vce02392', // Clé de test pour le développement
-  googleClientId: 'MEME_ID_CLIENT_QUE_DEVELOPMENT', // Même ID client pour production
+  stripePublicKey: 'pk_test_51SU6lA8RbHgy6D1m3Litx37vWguQEYbYkSu5V7IAvoDHUVkslR0SwDPvx82hufR4Ol3PqFINEOh2GeQ6Ijt7jHC000Vce02392',
+  googleClientId: 'MEME_ID_CLIENT_QUE_DEVELOPMENT',
 };
